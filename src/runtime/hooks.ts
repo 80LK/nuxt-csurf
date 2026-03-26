e683e3ecd1d53f7413e6e8ee1dd8f6eaf6bb568c
80LK <80lkru@gmail.com>
2026-03-26 14:02:39 +0300

    fix lint

--- src/runtime/hooks.ts
+++ src/runtime/hooks.ts
@@ -2,7 +2,7 @@ import type { FetchContext, ResponseType } from 'ofetch'
 
 export function onRequestBuilder(contextBuilder: () => ({ csrf: string, headerName: string })): (context: FetchContext<any, ResponseType>) => Promise<void> | void {
   return ({ options }) => {
-    const { csrf, headerName } = contextBuilder();
+    const { csrf, headerName } = contextBuilder()
 
     if (!options.headers || !(options.headers instanceof Headers)) {
       options.headers = new Headers(options.headers || {})
